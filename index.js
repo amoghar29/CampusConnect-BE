c765b1d25bcc51323a4b0c2ff5be576a770ec7b8
amogha r <amoghar23@gmail.com>
2024-12-29 18:05:00 +0530

    not much changes

--- index.js
+++ index.js
@@ -1,49 +1,41 @@
+const dotenv = require("dotenv");
 const express = require("express");
-require("dotenv").config({ path: "../.env" });
-// const connectToDb  = require("./connection");
-const feedbackRouter = require("./routes/feedback"); // Feedback Router
-const suggestionRouter = require("./routes/suggestion");
-const authRouter = require("./routes/auth");
-const cors = require('cors');
-const app = express();
-const Router = express.Router(); // Main router
+const { connectToDb } = require("./connection");
+const cors = require("cors");
+// const cookieParser = require("cookie-parser");
+const routes = require("./routes/index");
 const mongodbUrl = process.env.MONGODB_URL;
-const mongoose = require('mongoose')
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+//connection to db
+connectToDb(mongodbUrl).then(() => console.log("connect to Db"));
+
 // Middleware
 const allowedOrigins = [
-  'http://localhost:5173', // Local development
-  'https://campus-connect-fe.vercel.app' // Deployed frontend
+  "http://localhost:5173", // Local development
+  "https://campus-connect-fe.vercel.app", // Deployed frontend
 ];
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, origin);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true // Allow credentials
-}));
-app.use(express.json()); // Parse JSON body
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allow credentials
+  })
+);
 
-// Connect to the database
-// connectToDb(mongodbUrl)
-//   .then(() => console.log("Connected to DB"))
-//   .catch((err) => console.error("DB connection failed:", err));
+app.use(express.json());
+// app.use(cookieParser());
 
-mongoose.connect(mongodbUrl)
-// Attach feedback routes to the router
-Router.use("/feedback", feedbackRouter);
-Router.use("/suggestion", suggestionRouter);
-Router.use("/", authRouter);
-// Use the router in the app
-app.use(Router);
+app.use("/api/", routes);
 
-// Start the server
-const PORT = process.env.PORT || 4000; // Default to 4000 if PORT is not set
 app.listen(PORT, () => {
-  // console.log(`Server started at port ${PORT}`); // Log the actual port being used
-}).on('error', (err) => {
-  // console.error("Server failed to start:", err); // Log any startup errors
+  console.log(`Server is running on port ${PORT}`);
 });
