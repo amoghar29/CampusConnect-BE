f2e42cacf7f266acacaaf51f92e77c6966231b85
amogha r <amoghar23@gmail.com>
2025-01-18 01:24:47 +0530

    upadated cors

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cookieParser = require("cookie-parser");
 const routes = require("./routes/index");
 
 const mongodbUrl = process.env.MONGODB_URL;
-const FRONTEND_URL = process.env.FRONTEND_URL;
 const PORT = process.env.PORT || 4000;
 const app = express();
 
@@ -14,25 +13,28 @@ connectToDb(mongodbUrl).then(() => console.log("Connected to DB"));
 
 // Middleware
 const corsOptions = {
-  origin:
-    process.env.NODE_ENV === "production"
-      ? FRONTEND_URL
-      : "http://localhost:5173",
+  origin: [
+    "http://localhost:5173", // Development
+    "https://campus-connect-fe.vercel.app", // Production
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   exposedHeaders: ["set-cookie"],
 };
 
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
 
 // CORS headers middleware
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://campus-connect-fe.vercel.app"
+  );
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
